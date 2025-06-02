80626bad658323afd8b9cac1cd4eca3521fa6c20
Moro <hound90@mail.ru>
2025-06-02 19:07:26 +0500

    ПР 5 спринта выполнена

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
+
+function cardCreate (card, cardDelete) {
+   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+   const buttonDelete = cardElement .querySelector('.card__delete-button');
+
+   cardElement.querySelector('.card__title').textContent = card.name;
+   cardElement.querySelector('.card__image').src = card.link;
+   cardElement.querySelector('.card__image').alt = card.name;
+
+   buttonDelete.addEventListener ('click', () => {cardDelete(cardElement)}); 
+
+   return cardElement;
+}
+
+function cardDelete(cardElement) {
+   cardElement.remove();
+}
+
+initialCards.forEach((item) => {
+   const card = cardCreate(item, cardDelete);
+   cardList.append(card);
+})
+
+ 
